2eaa913a283fcac74c71cefa75ec4d70ce5b97b2
Grandeman <joshuajmorales01@gmail.com>
2023-07-06 17:32:03 -0400

    Coded all outcomes of playing round

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let choices = ['Rock', 'Paper', 'Scissors'];
+let choices = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice () {
     let randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-console.log(getComputerChoice());
 
-let playerSelection = prompt("Choose rock, paper, or scissors",'');
-
-function play(playerSelection, computerSelection){
-    computerSelection = getComputerChoice().toLowerCase();
-    playerSelection= playerSelection.toLowerCase();
-
-    if (playerSelection === computerSelection) {
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection) {
         return "It's a tie!";
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return "You lose! Paper beats rock"}
+        return "You lose! Paper beats rock";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose! Scissors beats paper";
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose! Rock beats scissors";
+    } else return "You win!";
 }
 
-const computerSelection= getComputerChoice();
-console.log(play(playerSelection, computerSelection));
+let playerSelection = prompt("Choose rock, paper, or scissors",'').toLowerCase();
+let computerSelection= getComputerChoice().toLowerCase();
 
+console.log(computerSelection);
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerSelection));
