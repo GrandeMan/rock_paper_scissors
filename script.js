ec4ac0f17cf048c747973cfea74f6b48d20f3a0e
Grandeman <joshuajmorales01@gmail.com>
2023-07-05 21:25:05 -0400

    Coded tie and paper vs rock

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function getComputerChoice () {
     return choices[randomIndex];
 }
 console.log(getComputerChoice());
+
+let playerSelection = prompt("Choose rock, paper, or scissors",'');
+
+function play(playerSelection, computerSelection){
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection= playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose! Paper beats rock"}
+}
+
+const computerSelection= getComputerChoice();
+console.log(play(playerSelection, computerSelection));
+
